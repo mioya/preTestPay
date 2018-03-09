e0350147e6b44e9a61f3a8b405d3a1bda0049506
mio <powalsld@naver.com>
2018-03-09 22:59:44 +0900

    dev:프로젝트 초기 세팅 - issued coupon  도메인 추가 - jpa 프로퍼티, h2 프로퍼티 추가

---
+++ src/main/java/com/kakaopay/model/IssuedCoupon.java
@@ -0,0 +1,35 @@
+package com.kakaopay.model;
+
+import lombok.*;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.*;
+import java.util.Date;
+
+/**
+ * Created by mio on 2018. 3. 9..
+ */
+@Entity
+@Table(name = "issued_coupon")
+@NoArgsConstructor
+@Setter
+@Getter
+@EqualsAndHashCode
+@ToString
+public class IssuedCoupon {
+
+    @Id
+    @GeneratedValue(generator = "system-uuid")
+    @GenericGenerator(name = "system-uuid", strategy = "uuid")
+    @Column(name = "id")
+    private String id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "coupon_number")
+    private String couponNumber;
+
+    @Column(name = "created_dt", updatable = false)
+    private Date createdDt;
+}
